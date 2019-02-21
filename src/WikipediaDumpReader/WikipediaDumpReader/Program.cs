d4e8c67f3455e8f60acbb17fa5e219f36f7cff95
Marcus Lager <marcus.lager@avensiastorefront.com>
2019-02-21 17:34:51 +0100

    fix reading from wiki index dump

--- src/WikipediaDumpReader/WikipediaDumpReader/Program.cs
+++ src/WikipediaDumpReader/WikipediaDumpReader/Program.cs
@@ -1,6 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
-using System.Text;
 using ICSharpCode.SharpZipLib.BZip2;
 using Newtonsoft.Json;
 
@@ -32,16 +36,25 @@ namespace WikipediaDumpReader
 
                 using (var targetStream = File.Create(target))
                 using (var writer = new StreamWriter(targetStream))
-                using (var stream = File.OpenRead(fileName))
-                using (var reader = new StreamReader(stream, Encoding.Unicode))
+                using (var fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read, 4096))
+                using (var stream = new BufferedStream(fs, 4096))
+                using (var reader = new StreamReader(stream))
                 {
+                    Console.WriteLine("attempting to read first line");
+
                     var line = reader.ReadLine();
+
+                    Console.WriteLine("read first line");
 
                     while (!string.IsNullOrWhiteSpace(line))
                     {
                         line = reader.ReadLine();
 
-                        if (line.StartsWith("]") || read++ == count)
+                        read++;
+
+                        Console.WriteLine("read line " + read);
+
+                        if (line.StartsWith("]") || read == count)
                         {
                             break;
                         }
@@ -51,50 +64,74 @@ namespace WikipediaDumpReader
                         }
 
                         var jsonObj = JsonConvert.DeserializeObject(line);
-                        var formatted = JsonConvert.SerializeObject(jsonObj, Formatting.Indented);
+                        var formatted = JsonConvert.SerializeObject(jsonObj, Formatting.None);
 
                         writer.Write(formatted);
+                        writer.WriteLine();
                     }
                 }
             }
             else if (action == "submit")
             {
-                var fileName = args[1];
                 var url = args[2];
-                var count = int.Parse(args[3]);
-                var read = 0;
 
-                using (var stream = File.OpenRead(fileName))
-                using (var reader = new StreamReader(stream, Encoding.Unicode))
+                foreach (var batch in ReadFile(args).Batch(1000))
                 {
-                    var line = reader.ReadLine();
-
-                    while (!string.IsNullOrWhiteSpace(line))
-                    {
-                        line = reader.ReadLine();
-
-                        if (line.StartsWith("]") || read++ == count)
-                        {
-                            break;
-                        }
-                        else if (line.StartsWith("["))
-                        {
-                            continue;
-                        }
-
-                        Submit(JsonConvert.DeserializeObject(line), url);
-                    }
+                    Submit(
+                        batch.Where(x => x.Contains("title")).Select(x => new Dictionary<string, object>
+                            {
+                                { "language", x["language"].ToString() },
+                                { "site", "en.wikipedia.org" },
+                                { "_url", string.Format("www.wikipedia.org/search-redirect.php?family=wikipedia&language={0}&search={1}", x["language"], x["title"]) },
+                                { "title", x["title"] },
+                                { "body", x["text"] },
+                                { "_created", DateTime.Now.ToBinary() }
+                            }), 
+                        url);
                 }
             }
         }
 
-        private static void Submit(object document, string url)
+        private static IEnumerable<IDictionary> ReadFile(string[] args)
         {
+            var fileName = args[1];
+            var count = int.Parse(args[3]);
+            var read = 0;
+
+            using (var stream = File.OpenRead(fileName))
+            using (var reader = new StreamReader(stream))
+            {
+                var line = reader.ReadLine();
+
+                while (!string.IsNullOrWhiteSpace(line))
+                {
+                    line = reader.ReadLine();
+
+                    read++;
+
+                    if (line.StartsWith("]") || read == count)
+                    {
+                        break;
+                    }
+                    else if (line.StartsWith("["))
+                    {
+                        continue;
+                    }
+
+                    yield return JsonConvert.DeserializeObject<IDictionary>(line);
+                }
+            }
+        }
+
+        private static void Submit(IEnumerable<object> documents, string url)
+        {
+            var time = Stopwatch.StartNew();
+
             var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = "POST";
 
-            var json = JsonConvert.SerializeObject(document);
+            var json = JsonConvert.SerializeObject(documents);
 
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
@@ -106,6 +143,38 @@ namespace WikipediaDumpReader
             {
                 var result = streamReader.ReadToEnd();
             }
+
+            Console.WriteLine("submit took {0}", time.Elapsed);
+        }
+    }
+
+    public static class ListHelper
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(
+        this IEnumerable<T> source, int size)
+        {
+            T[] bucket = null;
+            var count = 0;
+
+            foreach (var item in source)
+            {
+                if (bucket == null)
+                    bucket = new T[size];
+
+                bucket[count++] = item;
+
+                if (count != size)
+                    continue;
+
+                yield return bucket;
+
+                bucket = null;
+                count = 0;
+            }
+
+            // Return the last bucket with all remaining elements
+            if (bucket != null && count > 0)
+                yield return bucket.Take(count);
         }
     }
 }
